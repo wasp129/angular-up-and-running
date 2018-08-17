1ebeb809abdb0d4712a1431e434a1df7a66a1083
Andreas Schultz <andreasschultz@hotmail.com>
2018-08-17 13:38:34 +0200

    Initial commit

---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 1, name: 'Superman'},
+	{ id: 2, name: 'Batman'},
+	{ id: 3, name: 'Spider-Man'},
+	{ id: 4, name: 'Deadpool'},
+	{ id: 5, name: 'Shazam'},
+	{ id: 6, name: 'Aquaman'},
+	{ id: 7, name: 'Wonder Woman'},
+	{ id: 8, name: 'Iron Man'},
+	{ id: 9, name: 'Hulk'},
+	{ id: 10, name: 'Thor'}
+]
